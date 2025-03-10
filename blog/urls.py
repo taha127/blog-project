be05d995631d940180f6cb5e9bf2d9a16e33256b
taha127 <tahadehghani127@gmail.com>
2025-03-10 10:32:24 +0330

    Create url path & view for each detail blog.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list_view, name='posts_list'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 def post_list_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status='pub')
     return render(request, 'blog/posts_list.html',
                   {'posts_list': posts})
+def post_detail_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except ObjectDoesNotExist:
+    #     post = None
+    #     print("Excepted")
+    return render(request, 'blog/post_detail.html',
+                  {'post': post})
